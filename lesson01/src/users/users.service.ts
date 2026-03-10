ee6beb696547239a8ae95ba38ce11b724e0a036d
Juvraj-Talentica <juvraj@talentica.com>
2026-03-10 20:44:25 +0530

    Enhance UsersService with error handling for findAll and findOne methods

--- lesson01/src/users/users.service.ts
+++ lesson01/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
@@ -14,13 +14,22 @@ export class UsersService {
 
     findAll(role?: 'INTERN' | 'EMPLOYEE' | 'MANAGER' | 'ADMIN') {
         if (role) {
-            return this.users.filter(user => user.role === role);
+            const rolesArray = this.users.filter(user => user.role === role);
+            if(rolesArray.length === 0) {
+                throw new NotFoundException(`No users with role ${role} found`);
+            }
+            return rolesArray;
         }   
         return this.users;
     }
 
     findOne(id: number) {
-        return this.users.find(user => user.id === id);
+        const user = this.users.find(user => user.id === id); 
+        if(!user) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
+
+        return user
     }   
 
     create(createUserDto: CreateUserDto) {
