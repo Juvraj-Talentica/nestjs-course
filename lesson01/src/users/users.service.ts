347f0482408c3cb2fc85c0b980bd2e1886166b77
Juvraj-Talentica <juvraj@talentica.com>
2026-03-10 17:46:16 +0530

    Implement UsersController and UsersService with CRUD operations

--- lesson01/src/users/users.service.ts
+++ lesson01/src/users/users.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        { id: 1, name: 'Alice', email: 'alice@example.com', role: 'EMPLOYEE' },
+        { id: 2, name: 'Bob', email: 'bob@example.com', role: 'MANAGER' },
+        { id: 3, name: 'Charlie', email: 'charlie@example.com', role: 'INTERN' },
+        { id: 4, name: 'Diana', email: 'diana@example.com', role: 'EMPLOYEE' },
+        { id: 5, name: 'Eve', email: 'eve@example.com', role: 'MANAGER' },
+    ]
+
+    findAll(role?: 'INTERN' | 'EMPLOYEE' | 'MANAGER' | 'ADMIN') {
+        if (role) {
+            return this.users.filter(user => user.role === role);
+        }   
+        return this.users;
+    }
+
+    findOne(id: number) {
+        return this.users.find(user => user.id === id);
+    }   
+
+    create(user: { name: string, email: string, role: 'INTERN' | 'EMPLOYEE' | 'MANAGER' | 'ADMIN' }) {
+        const highestId = this.users.reduce((maxId, user) => Math.max(maxId, user.id), 0);
+        const newUser = { id: highestId + 1, ...user };
+        this.users.push(newUser);
+        return newUser;
+    } 
+
+    update(id: number, userUpdate: { name?: string, email?: string, role?: 'INTERN' | 'EMPLOYEE' | 'MANAGER' | 'ADMIN' }) {
+        const user = this.findOne(id);  
+        if (!user) {
+            return null;
+        }
+        Object.assign(user, userUpdate);
+        return user;
+    }
+
+    delete(id: number) {
+        const deletedUser = this.findOne(id);
+        if (!deletedUser) {
+            return null;
+        }
+        this.users = this.users.filter(user => user.id !== id);
+        return deletedUser;
+    }
+
+}
